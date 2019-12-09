b80210a9f175ad30d0ae5c07f49c92b4ee461472
Anmol Arora <anmolarora1711@gmail.com>
2019-12-09 12:59:18 +0530

    Update game-2.py

--- game-2.py
+++ game-2.py
@@ -1,8 +1,16 @@
 import pygame
 pygame.init()
 
-screenWidth = 1000
-screenHeight = 600
+counter = 0
+
+def displayScore():
+    msg = f"Score : {counter}"
+    font = pygame.font.SysFont(None, 25, True, True)
+    score = font.render(msg, True, red, None)
+    screen.blit(score, (0,0))
+
+screenWidth = 500
+screenHeight = 300
 
 # Color - red,green,blue (primary colors) 0-255
 red = 255,0,0
@@ -15,9 +23,29 @@ x = 0
 y = 0
 rectWidth = 50
 rectHeight = 50
-radius = 25
-moveX = 1
-moveY = 1
+radius = 10
+moveX = 0
+moveY = 0
+moveBarX = 0
+barWidth = 150
+barHeight = 10
+ballX, ballY = screenWidth//2, screenHeight - 20 - radius
+barX, barY = screenWidth//2 - barWidth // 2, screenHeight - 20
+ballShouldMoveWithBar = True
+
+noOfBricks = 10
+bricksList = []
+widthAvailableForBricks = screenWidth - (radius * (noOfBricks + 1))
+print(widthAvailableForBricks)
+brickSize = widthAvailableForBricks / noOfBricks
+print(brickSize)
+brickHeight = 25
+
+for i in range(4):
+    for j in range(noOfBricks):
+        bricksList.append(((brickSize + 10) * j, 35 * i))
+
+print(bricksList)
 
 screen = pygame.display.set_mode((screenWidth,screenHeight))
 
@@ -29,21 +57,60 @@ while True:
             quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                moveX = -1
-            elif event.key == pygame.K_UP:
-                moveY = -1
-            elif event.key == pygame.K_DOWN:
-                moveY = 1
+                moveBarX = -1
+                if ballShouldMoveWithBar:
+                    moveX = -1
             elif event.key == pygame.K_RIGHT:
+                moveBarX = 1
+                if ballShouldMoveWithBar:
+                    moveX = 1
+            elif event.key == pygame.K_SPACE:
+                moveY = -1
                 moveX = 1
+                ballShouldMoveWithBar = False
+            # elif event.key == pygame.K_UP:
+            #     moveY = -1
+            # elif event.key == pygame.K_DOWN:
+            #     moveY = 1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moveBarX = 0
+                if ballShouldMoveWithBar:
+                    moveX = 0
+            elif event.key == pygame.K_RIGHT:
+                moveBarX = 0
+                if ballShouldMoveWithBar:
+                    moveX = 0
 
     screen.fill(white)
 
     # place to draw, color, (x,y,rectWidth,rectHeight)
     # pygame.draw.rect(screen, newColor, (x,y,rectWidth,rectHeight))
-    pygame.draw.circle(screen, newColor, (x,y),radius)
-    x = x + moveX
-    y = y + moveY
+    barRect = pygame.draw.rect(screen, blue, (barX, barY, barWidth, barHeight))
+    ballRect = pygame.Rect((ballX-radius, ballY-radius,radius*2,radius*2))
+    # pygame.draw.rect(screen,newColor,(ballX-radius, ballY-radius,radius*2,radius*2))
+    ball = pygame.draw.circle(screen, red, (ballX, ballY),radius)
+    barX = barX + moveBarX
+    ballX = ballX + moveX
+    ballY = ballY + moveY
+
+    if barRect.colliderect(ballRect):
+        moveY = -1
+        
+
+    for i,brick in enumerate(bricksList):
+        brickRect = pygame.draw.rect(screen, green, (brick[0] + 10, brick[1] + 10, brickSize, brickHeight))
+        if brickRect.colliderect(ballRect):
+            moveY = 1
+            counter += 1
+            del bricksList[i]
+            break
+
+    displayScore()
+
+    # x = x + moveX
+    # y = y + moveY
 
     # rectangle conditions
     # if y > screenHeight - rectHeight:
@@ -55,13 +122,13 @@ while True:
     # elif x < 0:
     #     moveX = 1
 
-    if y > screenHeight - radius:
-        moveY = -1
-    elif x > screenWidth - radius:
+    # if ballY > screenHeight - radius:
+    #     moveY = -1
+    if ballX > screenWidth - radius:
         moveX = -1
-    elif y < radius:
+    elif ballY < radius:
         moveY = 1
-    elif x < radius:
+    elif ballX < radius:
         moveX = 1
 
     pygame.display.update()    
